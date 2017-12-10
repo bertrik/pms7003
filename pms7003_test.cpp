4b424ae29d8c01f5a9472880b92ab4ed97146308
Bertrik Sikken <bertrik@sikken.nl>
2017-12-10 04:29:17 +0100

    Simplify unit test a bit

--- pms7003_test.cpp
+++ pms7003_test.cpp
@@ -16,53 +16,43 @@ static bool assertEquals(int expected, int actual, const char *field)
 
 static bool test_rx(void)
 {
-    uint8_t data[32];
-    pms_meas_t meas;
+    uint8_t frame[] = {
+        // some garbage bytes
+        0x00, 0xFF, 0x42,
+        // header, length
+        0x42, 0x4D, 0x00, 0x1C,
+        // CF1
+        0x00, 0x10, 0x00, 0x25, 0x01, 0x00,
+        // amb
+        0x00, 0x10, 0x00, 0x25, 0x01, 0x00,
+        // raw
+        0x00, 0x03, 0x00, 0x05, 0x00, 0x10,
+        0x00, 0x25, 0x00, 0x50, 0x01, 0x00,
+        // version, errorcode
+        0x88, 0x99,
+        // checksum
+        0x02, 0xC6
+    };
 
-    PmsInit(data, sizeof(data));
-
-    // send some garbage bytes
-    PmsProcess(0x00);
-    PmsProcess(0xFF);
-    PmsProcess(0x42);
-
-    // send the actual frame
-    PmsProcess(0x42); PmsProcess(0x4D);
-    PmsProcess(0x00); PmsProcess(0x1C);
-    
-    PmsProcess(0x00); PmsProcess(0x10);
-    PmsProcess(0x00); PmsProcess(0x25);
-    PmsProcess(0x01); PmsProcess(0x00);
-    
-    PmsProcess(0x00); PmsProcess(0x10);
-    PmsProcess(0x00); PmsProcess(0x25);
-    PmsProcess(0x01); PmsProcess(0x00);
-    
-    PmsProcess(0x00); PmsProcess(0x03);
-    PmsProcess(0x00); PmsProcess(0x05);
-    PmsProcess(0x00); PmsProcess(0x10);
-    PmsProcess(0x00); PmsProcess(0x25);
-    PmsProcess(0x00); PmsProcess(0x50);
-    PmsProcess(0x01); PmsProcess(0x00);
-    
-    PmsProcess(0x88);
-    PmsProcess(0x99);
-    
-    // checksum
-    PmsProcess(0x02);
-    bool ok = PmsProcess(0xC6);
+    // send frame data
+    bool ok;
+    uint8_t rxdata[32];
+    PmsInit(rxdata, sizeof(rxdata));
+    for (size_t i = 0; i < sizeof(frame); i++) {
+        ok = PmsProcess(frame[i]);
+    }
     if (!ok) {
         fprintf(stderr, "expected successful frame!");
         return false;
     }
     
     // parse
+    pms_meas_t meas;
     PmsParse(&meas);
     ok = assertEquals(0x0010, meas.concPM1_0_CF1, "concPM1_0_CF1");
     ok = ok && assertEquals(0x0025, meas.concPM2_5_CF1, "concPM2_5_CF1");
     ok = ok && assertEquals(0x25, meas.concPM2_5_amb, "concPM2_5_amb");
     ok = ok && assertEquals(0x50, meas.rawGt5_0um, "rawGt5_0um");
-    
     ok = ok && assertEquals(0x88, meas.version, "version");
     ok = ok && assertEquals(0x99, meas.errorCode, "errorCode");
 
